b6365aee395f61c93f1ea6228bb640e694d9e126
Ignacio Losiggio <iglosiggio@gmail.com>
2017-06-21 17:39:11 -0300

    Agregada documentación al ejemplo

--- rcli.example.js
+++ rcli.example.js
@@ -1,13 +1,19 @@
-const rcli = require('./rcli')
+const rcli = require('./rcli');
+
+/* El handler de un comando recibe tres parámetros:
+ *   * data: un objeto con la data pasada al comando
+ *   * write: una función que le permite escribir en el socket
+ *   * done: una función que DEBE llamar cuando ha terminado de escribir en el socket
+ */
 
 const server = rcli({
-  echo(data, write, callback) {
-    write(data.toString());
-    callback();
-  }
+  echo(data, write, done) {
+    write(data);
+    done();
+  },
 
-  hola(data, write, callback) {
+  hola(data, write, done) {
     write(`Hola ${data.quien}!`);
-    callback();
+    done();
   }
 }).listen(1337);
